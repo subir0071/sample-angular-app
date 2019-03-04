fa4650d4a64eaf176c67dc2040173ad663ecdcbb
Sourabh Gupta <gupta.sourabh385@gmail.com>
2019-03-04 11:48:39 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,4 @@
+var server = app.listen(process.env.PORT || 8079, function () {
+  var port = server.address().port;
+  console.log("App now running in %s mode on port %d", app.get("env"), port);
+});
