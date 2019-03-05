98c6952cc443b08a137e1216f17530cbbff99f7d
Sourabh Gupta <gupta.sourabh385@gmail.com>
2019-03-05 10:40:21 +0530

    Delete server.js

--- server.js
+++
@@ -1,4 +0,0 @@
-var server = app.listen(process.env.PORT || 8079, function () {
-  var port = server.address().port;
-  console.log("App now running in %s mode on port %d", app.get("env"), port);
-});
