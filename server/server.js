fbec0606994579ff8e64aae54cd5bdd24ac7b970
Sourabh Gupta <gupta.sourabh385@gmail.com>
2019-03-05 11:33:48 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,10 +4,10 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, '../dist')));
+app.use(express.static(path.join(__dirname, '../dist/my-app')));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '../dist/my-app', 'index.html'));
 });
 
 app.listen(PORT);
