02868180006c22aef335947a4389db986f72e1a0
Sourabh Gupta <gupta.sourabh385@gmail.com>
2019-04-26 15:02:08 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'my-app';
   company = 'ADMCOE';
-  branch= 'developer1 is sourabh';
+  branch= 'developer1 is sourabh gupta';
 }
